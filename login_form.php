984b296bdba05a2f76bd20052a3b82c92be85e2b
Bogita Brian <bogitabrian@gmail.com>
2023-09-20 12:35:24 +0300

    styled all forms using css

---
+++ login_form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Login Here</title>
+    <link rel="stylesheet" href="styles.css"
+</head>
+<body>
+    <div class="form-container">
+        <form action="" method="post">
+            <h3>Login Now</h3>
+            
+            <input type="email" name="email" required placeholder="Enter your email">
+            <input type="password" name="password" required placeholder="Enter your password">
+            
+            
+            <input type="submit" name="submit" value="login now" class="form-btn">
+            <p>don't have an account? <a href="register_form.php">Register now</a></p>
+        </form>
+    </div>
+    
+</body>
+</html>
